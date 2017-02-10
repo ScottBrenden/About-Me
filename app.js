3f1b91d95003422bd231ba81d1e53833f3fb6f83
Enrique Rico <enriquesantosrico@gmail.com>
2017-02-10 14:59:03 -0800

    Updated with functions

--- app.js
+++ app.js
@@ -9,106 +9,112 @@ if(confirmQuiz){
 }
 //-----------------------Questions 1-5: Y/N GAME-------------------V
 var user = prompt('Now that you agreed to play, what is your name?');
-var YorN = alert('Ok, ' + user + ' the following questions are yes/no questions. Please answer each with Y or N. Otherwise you may be subject to ridicule.');
+function questionsOneToFive(){
+  var YorN = alert('Ok, ' + user + ' the following questions are yes/no questions. Please answer each with Y or N. Otherwise you may be subject to ridicule.');
 
-var questions = ['Is Scott a ginger?', 'Has Scott ever lived outside the great state the is Washington?', 'Does Scott ski or snowboard (Y for ski, N for snowboard)? Ok, this is\'t yes/no. My game, my rules. Suck it up ' + user + '.', 'Is Scott a sports fan?', 'Did Scott Graduate from WWU?'];
-var Ys = ['Yup! Lucky guy.', 'No, why would he? Washington is clearly the best place to live.', 'Yup! He has even done it professionally .', 'Oh yeah! Probably a bit too much.', 'Yup with a BS! The \'a\' is important there.'];
-var Ns = ['No, he is. If you are color blind I won\'t count this point.', 'You are corrcet! Unless you submitted something other than N, in which case you failed and have been launched into the pit of dispair.', 'Ok, it was a trick question he does both.', 'Nope, he is. Well except soccer.', 'No, he did. Go Vikings!'];
-var answers = [];
+  var questions = ['Is Scott a ginger?', 'Has Scott ever lived outside the great state the is Washington?', 'Does Scott ski or snowboard (Y for ski, N for snowboard)? Ok, this is\'t yes/no. My game, my rules. Suck it up ' + user + '.', 'Is Scott a sports fan?', 'Did Scott Graduate from WWU?'];
+  var Ys = ['Yup! Lucky guy.', 'No, why would he? Washington is clearly the best place to live.', 'Yup! He has even done it professionally .', 'Oh yeah! Probably a bit too much.', 'Yup with a BS! The \'a\' is important there.'];
+  var Ns = ['No, he is. If you are color blind I won\'t count this point.', 'You are corrcet! Unless you submitted something other than N, in which case you failed and have been launched into the pit of dispair.', 'Ok, it was a trick question he does both.', 'Nope, he is. Well except soccer.', 'No, he did. Go Vikings!'];
+  var answers = [];
 
-for (var i = 0; i < questions.length; i++) {
-  var response = prompt(questions[i]);
-  var done = false;
-  while (!done) {
-    if(response.toUpperCase() === 'Y'){
-      alert(Ys[i]);
-      console.log(Ys[i]);
-      answers += [Ys[i]];
-      done = true;
-    } else if (response.toUpperCase() === 'N') {
-      alert(Ns[i]);
-      console.log(Ns[i]);
-      answers += [Ns[i]];
-      done = true;
-    } else {
-      alert('Y or N means Y or N ' + user + '! You pleb.');
-      console.log('Y or N means Y or N ' + user + '! You pleb.');
-      response = prompt('Please answer Y or N. ' + questions[i]);
+  for (var i = 0; i < questions.length; i++) {
+    var response = prompt(questions[i]);
+    var done = false;
+    while (!done) {
+      if(response.toUpperCase() === 'Y'){
+        alert(Ys[i]);
+        console.log(Ys[i]);
+        answers += [Ys[i]];
+        done = true;
+      } else if (response.toUpperCase() === 'N') {
+        alert(Ns[i]);
+        console.log(Ns[i]);
+        answers += [Ns[i]];
+        done = true;
+      } else {
+        alert('Y or N means Y or N ' + user + '! You pleb.');
+        console.log('Y or N means Y or N ' + user + '! You pleb.');
+        response = prompt('Please answer Y or N. ' + questions[i]);
+      }
     }
   }
 }
-
+questionsOneToFive();
 //----------------Question 6: NUMBER GAME!----------------------V
-var guesses = 1;
-var correct = false;
+function numGame(){
+  var guesses = 1;
+  var correct = false;
 
-while(!correct && guesses < 5){
-  var remaining = 5 - guesses;
-  var question6 = prompt('Alright, enough yes or no! How many years has scott been skiing for? You have ' + remaining + ' chances... no pressure.');
-  if (question6 == 21){
-    if (guesses == 1) {
-      console.log('First guess! Either you know Scott, or we should hit up Vegas.');
-      alert('First guess! Either you know Scott, or we should hit up Vegas.');
+  while(!correct && guesses < 5){
+    var remaining = 5 - guesses;
+    var question6 = prompt('Alright, enough yes or no! How many years has scott been skiing for? You have ' + remaining + ' chances... no pressure.');
+    if (question6 == 21){
+      if (guesses == 1) {
+        console.log('First guess! Either you know Scott, or we should hit up Vegas.');
+        alert('First guess! Either you know Scott, or we should hit up Vegas.');
+      } else {
+        console.log('You got it!');
+        alert('You got it!');
+      }
+      correct = true;
+    } else if (question6 < 21) {
+      if (guesses == 4) {
+        console.log('Guess higher!');
+        alert('Guess higher!');
+      } else {
+        console.log('Guess higher! Last chance!');
+        alert('Guess higher! Last chance!');
+      }
+      guesses++;
+    } else if (question6 > 21) {
+      if (guesses == 4) {
+        console.log('Guess lower.');
+        alert('Guess lower.');
+      } else {
+        console.log('Guess lower. Last chance!');
+        alert('Guess lower. Last chance!');
+      }
+      guesses++;
     } else {
-      console.log('You got it!');
-      alert('You got it!');
+      console.log(question6 + ' is not a number...');
+      alert(question6 + ' is not a number...');
+      guesses++;
     }
-    correct = true;
-  } else if (question6 < 21) {
-    if (guesses == 4) {
-      console.log('Guess higher!');
-      alert('Guess higher!');
-    } else {
-      console.log('Guess higher! Last chance!');
-      alert('Guess higher! Last chance!');
+    if (guesses == 5 && !correct){
+      console.log('Better luck next time.');
+      alert('Better luck next time.');
     }
-    guesses++;
-  } else if (question6 > 21) {
-    if (guesses == 4) {
-      console.log('Guess lower.');
-      alert('Guess lower.');
-    } else {
-      console.log('Guess lower. Last chance!');
-      alert('Guess lower. Last chance!');
-    }
-    guesses++;
-  } else {
-    console.log(question6 + ' is not a number...');
-    alert(question6 + ' is not a number...');
-    guesses++;
-  }
-  if (guesses == 5 && !correct){
-    console.log('Better luck next time.');
-    alert('Better luck next time.');
   }
 }
-
+numGame();
 //--------------------Question 7: MULTI ANSWER GAME-------------------V
-
-for (var i = 0; i < 5; i++){
-  var qustion7 = prompt('What is one country, outside North America, that Scott has been to?');
-  if(qustion7.toUpperCase() === 'NORWAY' || qustion7.toUpperCase() === 'CHILE' || qustion7.toUpperCase() === 'UK' || qustion7.toUpperCase() === 'GREAT BRITAIN'){
-    if(i == 0){
-      console.log('First guess! Nice!');
-      alert('First guess! Nice!');
+function multiGame(){
+  for (var i = 0; i < 5; i++){
+    var qustion7 = prompt('What is one country, outside North America, that Scott has been to?');
+    if(qustion7.toUpperCase() === 'NORWAY' || qustion7.toUpperCase() === 'CHILE' || qustion7.toUpperCase() === 'UK' || qustion7.toUpperCase() === 'GREAT BRITAIN'){
+      if(i == 0){
+        console.log('First guess! Nice!');
+        alert('First guess! Nice!');
+      } else {
+        console.log('Correct!');
+        alert('Correct!');
+      }
+      break;
     } else {
-      console.log('Correct!');
-      alert('Correct!');
-    }
-    break;
-  } else {
-    if (i == 4) {
-      console.log('Nope. Better luck next time.');
-      alert('Nope. Better luck next time.');
-    } else if (i == 3) {
-      console.log('Nope. One more try.');
-      alert('Nope. One more try.');
-    } else {
-      console.log('Incorrect.');
-      alert('Incorrect.');
+      if (i == 4) {
+        console.log('Nope. Better luck next time.');
+        alert('Nope. Better luck next time.');
+      } else if (i == 3) {
+        console.log('Nope. One more try.');
+        alert('Nope. One more try.');
+      } else {
+        console.log('Incorrect.');
+        alert('Incorrect.');
+      }
     }
   }
 }
+multiGame();
 // var quizResponse2 = prompt('Has Scott ever lived outside the great state the is Washington?');
 
 // if (quizResponse2.toUpperCase() === 'Y') {
